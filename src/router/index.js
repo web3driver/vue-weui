a1f30fd1291f9242e9e61641207dc01113d49b73
yvshuo <cuc_yushuo@qq.com>
2017-03-07 14:59:12 +0800

    save changes

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,13 @@ import Router from 'vue-router';
 
 // import pages
 import index from '@/pages/index';
+// 表单
 import button from '@/pages/button';
+// 基础组件
+
+// 导航相关
+import navbar from '@/pages/navbar';
+import tabbar from '@/pages/tabbar';
 
 Vue.use(Router);
 
@@ -14,9 +20,8 @@ export default new Router({
       name: 'index',
       component: index,
     },
-    {
-      path: '/button',
-      component: button,
-    },
+    { path: '/button', component: button },
+    { path: '/navbar', component: navbar },
+    { path: '/tabbar', component: tabbar },
   ],
 });
