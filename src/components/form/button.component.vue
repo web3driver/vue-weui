3fc24b791540144535178f62a60061298b1afab9
yvshuo <cuc_yushuo@qq.com>
2017-03-10 17:12:38 +0800

    update button component

--- src/components/form/button.component.vue
+++ src/components/form/button.component.vue
@@ -1,6 +1,38 @@
 <template>
+  <a href="javascript:;" class="weui-btn"
+    @click="handleClick"
+    :class="[
+        type&&!plain ? 'weui-btn_' + type : '',
+        type&&plain ? 'weui-btn_plain-' + type : '',
+        disabled&&!plain ? 'weui-btn_disabled' : '',
+        disabled&&plain ? 'weui-btn_plain-disabled' : '',
+        {
+            'weui-btn_loading': loading,
+            'weui-btn_mini': mini
+        }
+    ]"
+    >
+    <i class="weui-loading" v-if="loading"></i>
+    <slot></slot>
+  </a>
 </template>
 <script>
-    export default {
+  export default {
+    props: {
+      type: {
+        type: String,
+        default: 'default'
+      },
+      loading: Boolean,
+      disabled: Boolean,
+      plain: Boolean,
+      mini: Boolean
+    },
+
+    methods: {
+      handleClick(evt) {
+        this.$emit('click', evt);
+      }
     }
+  };
 </script>
--- src/pages/button.vue
+++ src/pages/button.vue
@@ -5,24 +5,24 @@
       <p class="page__desc">按钮</p>
     </div>
     <div class="page__bd page__bd_spacing">
-      <a href="javascript:;" class="weui-btn weui-btn_primary">页面主操作 Normal</a>
-      <a href="javascript:;" class="weui-btn weui-btn_primary weui-btn_loading"><i class="weui-loading"></i>页面主操作 Loading</a>
-      <a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_primary">页面主操作 Disabled</a>
-      <a href="javascript:;" class="weui-btn weui-btn_default">页面次要操作 Normal</a>
-      <a href="javascript:;" class="weui-btn weui-btn_default weui-btn_loading"><i class="weui-loading"></i>页面次操作 Loading</a>
-      <a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_default">页面次要操作 Disabled</a>
-      <a href="javascript:;" class="weui-btn weui-btn_warn">警告类操作 Normal</a>
-      <a href="javascript:;" class="weui-btn weui-btn_warn weui-btn_loading"><i class="weui-loading"></i>警告类操作 Loading</a>
-      <a href="javascript:;" class="weui-btn weui-btn_disabled weui-btn_warn">警告类操作 Disabled</a>
+      <yv-button type="primary">页面主操作 Normal</yv-button>
+      <yv-button type="primary" loading>页面主操作 Loading</yv-button>
+      <yv-button type="primary" disabled>页面主操作 Disabled</yv-button>
+      <yv-button type="default">页面次要操作 Normal</yv-button>
+      <yv-button type="default" loading>页面次要操作 Loading</yv-button>
+      <yv-button type="default" disabled>页面次要操作 Disabled</yv-button>
+      <yv-button type="warn">警告类操作 Normal</yv-button>
+      <yv-button type="warn" loading>警告类操作 Loading</yv-button>
+      <yv-button type="warn" disabled>警告类操作 Disabled</yv-button>
 
       <div class="button-sp-area">
-        <a href="javascript:;" class="weui-btn weui-btn_plain-default">按钮</a>
-        <a href="javascript:;" class="weui-btn weui-btn_plain-default weui-btn_plain-disabled">按钮</a>
-        <a href="javascript:;" class="weui-btn weui-btn_plain-primary">按钮</a>
-        <a href="javascript:;" class="weui-btn weui-btn_plain-primary weui-btn_plain-disabled">按钮</a>
-        <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_primary">按钮</a>
-        <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default">按钮</a>
-        <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_warn">按钮</a>
+        <yv-button type="default" plain>按钮</yv-button>
+        <yv-button type="default" plain disabled>按钮</yv-button>
+        <yv-button type="primary" plain>按钮</yv-button>
+        <yv-button type="primary" plain disabled>按钮</yv-button>
+        <yv-button type="primary" mini>按钮</yv-button>
+        <yv-button type="default" mini>按钮</yv-button>
+        <yv-button type="warn" mini>按钮</yv-button>
       </div>
     </div>
     <div class="page__ft">
