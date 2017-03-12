76781de7d75880f2f4e3cab869a7f7019e77d05a
yvshuo <cuc_yushuo@qq.com>
2017-03-12 23:24:59 +0800

    update toast

--- src/components/feedback/toast.component.vue
+++ src/components/feedback/toast.component.vue
@@ -1,6 +1,62 @@
 <template>
+  <transition name="fade">
+    <div v-show="display">
+      <div class="weui-mask_transparent"></div>
+      <div class="weui-toast">
+        <i class="weui-icon_toast" :class="iconType"></i>
+        <p class="weui-toast__content">{{textContent}}</p>
+      </div>
+    </div>
+  </transition>
 </template>
 <script>
-    export default {
+  export default {
+    props: {
+      type: {
+        type: String,
+        required: true,
+        validate: function (value) {
+          return value === 'success' || value === 'loading'
+        }
+      },
+      content: String,
+      display: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      iconType() {
+        if (this.type === 'success') {
+          return 'weui-icon-success-no-circle'
+        }
+        if (this.type === 'loading') {
+          return 'weui-loading'
+        }
+      },
+      textContent() {
+        if (this.content) {
+          return this.content
+        } else {
+          if (this.type === 'success') {
+            return '已完成'
+          } else if (this.type === 'loading') {
+            return '数据加载中'
+          }
+        }
+      }
     }
+  }
+
 </script>
+
+<style scoped>
+  .fade-leave-active {
+    transition: opacity .5s
+  }
+  
+  .fade-leave-active {
+    opacity: 0
+  }
+
+</style>
--- src/pages/toast.vue
+++ src/pages/toast.vue
@@ -1,36 +1,11 @@
 <template>
-  <div class="page js_show toast">
-    <div class="page__hd">
-      <h1 class="page__title">Toast</h1>
-      <p class="page__desc">弹出式提示</p>
-    </div>
-    <div class="page__bd page__bd_spacing">
-      <a href="javascript:;" class="weui-btn weui-btn_default" id="showToast">成功提示</a>
-      <a href="javascript:;" class="weui-btn weui-btn_default" id="showLoadingToast">加载中提示</a>
-    </div>
-    <div class="page__ft">
-      <a href="javascript:home()"><img src="./images/icon_footer_link.png" /></a>
-    </div>
+  <yv-page type="toast" title="Toast" name="弹出式提示">
+    <yv-button @click="showSuccess">成功提示</yv-button>
+    <yv-button @click="showLoading">加载中提示</yv-button>
 
-    <!--BEGIN toast-->
-    <div id="toast" style="display: none;">
-      <div class="weui-mask_transparent"></div>
-      <div class="weui-toast">
-        <i class="weui-icon-success-no-circle weui-icon_toast"></i>
-        <p class="weui-toast__content">已完成</p>
-      </div>
-    </div>
-    <!--end toast-->
-
-    <!-- loading toast -->
-    <div id="loadingToast" style="display:none;">
-      <div class="weui-mask_transparent"></div>
-      <div class="weui-toast">
-        <i class="weui-loading weui-icon_toast"></i>
-        <p class="weui-toast__content">数据加载中</p>
-      </div>
-    </div>
-  </div>
+    <yv-toast slot="mask" type="success" :display="show1"></yv-toast>
+    <yv-toast slot="mask" type="loading" :display="show2"></yv-toast>
+  </yv-page>
 </template>
 
 <script>
@@ -38,11 +13,29 @@
     name: 'toast',
     data() {
       return {
+        show1: false,
+        show2: false,
+        showTime: 1500
       };
     },
+    methods: {
+      showSuccess() {
+        this.show1 = true
+        setTimeout(() => {
+          this.show1 = false
+        }, this.showTime)
+      },
+      showLoading() {
+        this.show2 = true
+        setTimeout(() => {
+          this.show2 = false
+        }, this.showTime)
+      }
+    }
   };
 
 </script>
 
 <style scoped>
+
 </style>
