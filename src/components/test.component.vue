fc29a1b34eddadb98d7190156bde846b8a3824d2
yvshuo <cuc_yushuo@qq.com>
2017-03-24 13:32:02 +0800

    update

--- src/components/test.component.vue
+++ src/components/test.component.vue
@@ -7,6 +7,15 @@
 <script>
   export default {
     props: ['title'],
+    created() {
+      this.$http.get('https://cnodejs.org/api/v1/topics')
+      .then((res) => {
+        console.log(res);
+      }, (res) => {
+        console.log(res);
+        console.log('error');
+      });
+    },
     methods: {
       remove() {
         this.$emit('remove');
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import resource from 'vue-resource';
 import 'weui/dist/style/weui.min.css';
 import App from './App';
 import router from './router';
@@ -8,6 +9,8 @@ import components from './components';
 import page from './pages/templates/page.template';
 
 Vue.config.productionTip = false;
+
+Vue.use(resource);
 
 Object.keys(components).forEach((key) => {
   Vue.component(`yv-${key}`, components[key]);
